b5192a7c8c42be91845e3abea7ffc175cf158eea
Michael Orlov <overdzen@gmail.com>
2017-02-20 19:50:13 +0300

    Replace sqlite3 with postgre

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170218084815) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "clients", force: :cascade do |t|
     t.string   "phone_num"
     t.datetime "created_at", null: false
